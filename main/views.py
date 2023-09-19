5632f89f7da17fff969c831321adb24d8702ef1d
firedfrogg <julianalex2307@gmail.com>
2023-09-19 20:34:37 +0700

    Finished tugas 2

--- main/views.py
+++ main/views.py
@@ -25,15 +25,6 @@ def show_main(request):
         'item_count': item_count,
         'item_total_worth': item_total_worth,
         'days_since_added': latest_date - earliest_date
-        #'item_name': 'Pufferfish',
-        #'season': 'Summ`er',
-        #'favorable_weather': 'Sun',
-        #'price': 200,
-        #'description': 'The Pufferfish is a fish that can be caught in the ocean at The Beach or on the Beach Farm on '
-        #               'sunny summer days between 12pm and 4pm, or on Ginger Island during any season on Island West '
-        #               '(ocean), South, Southeast, and in the Pirate Cove. It can also randomly be found in Garbage '
-        #               'Cans during Summer, or at the Traveling Cart for 600–1,000g.',
-        #'amount': 1
     }
 
     return render(request, "main.html", context)
